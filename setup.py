0edd342eb1a5bb010d5155837ff1d3c2f7c859d7
Hanan Wiener <hanan888@gmail.com>
2018-10-01 13:02:32 +0300

    v0.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pika_client',
-      version='0.8.2',
+      version='0.9',
       description='Pika Client',
       url='https://github.com/BenjiBackslash/pika_client',
       author='Hanan Wiener',
